dd3fce0a3a6220411c705d080356974ae4c11ad4
meetsiraja <meetsiraja@gmail.com>
2021-08-22 01:22:22 +0530

    ft. UniswapV2ERC20 added permit functionality

--- contracts/test/uniswap/UniswapV2ERC20.sol
+++ contracts/test/uniswap/UniswapV2ERC20.sol
@@ -23,29 +23,29 @@ abstract contract UniswapV2ERC20 is IUniswapV2ERC20 {
     //     return test2;
     // }
 
-    // bytes32 public override DOMAIN_SEPARATOR;
-    // // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
-    // bytes32 public override constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
-    // mapping(address => uint) public override nonces;
+    bytes32 public DOMAIN_SEPARATOR;
+    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+    bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
+    mapping(address => uint) public nonces;
 
     // event Approval(address indexed owner, address indexed spender, uint value);
     // event Transfer(address indexed from, address indexed to, uint value);
 
-    // constructor() {
-    //     uint chainId;
-    //     // assembly {
-    //     //     chainId := chainid
-    //     // }
-    //     // DOMAIN_SEPARATOR = keccak256(
-    //     //     abi.encode(
-    //     //         keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
-    //     //         keccak256(bytes(name)),
-    //     //         keccak256(bytes('1')),
-    //     //         chainId,
-    //     //         address(this)
-    //     //     )
-    //     // );
-    // }
+    constructor() {
+        uint chainId;
+        assembly {
+            chainId := chainid()
+        }
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
+                keccak256(bytes(name)),
+                keccak256(bytes('1')),
+                chainId,
+                address(this)
+            )
+        );
+    }
 
     function _mint(address to, uint value) internal {
         totalSupply = totalSupply.add(value);
@@ -89,17 +89,17 @@ abstract contract UniswapV2ERC20 is IUniswapV2ERC20 {
         return true;
     }
 
-    // function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external override {
-    //     require(deadline >= block.timestamp, 'UniswapV2: EXPIRED');
-    //     bytes32 digest = keccak256(
-    //         abi.encodePacked(
-    //             '\x19\x01',
-    //             DOMAIN_SEPARATOR,
-    //             keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline))
-    //         )
-    //     );
-    //     address recoveredAddress = ecrecover(digest, v, r, s);
-    //     require(recoveredAddress != address(0) && recoveredAddress == owner, 'UniswapV2: INVALID_SIGNATURE');
-    //     _approve(owner, spender, value);
-    // }
+    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external {
+        require(deadline >= block.timestamp, 'UniswapV2: EXPIRED');
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                '\x19\x01',
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline))
+            )
+        );
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        require(recoveredAddress != address(0) && recoveredAddress == owner, 'UniswapV2: INVALID_SIGNATURE');
+        _approve(owner, spender, value);
+    }
 }
