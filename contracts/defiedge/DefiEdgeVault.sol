b45ddd9b761a741112c0811a6d4015c116ff8b1f
meetsiraja <meetsiraja@gmail.com>
2021-09-15 12:55:47 +0530

    ft. defiedge vault contracts

---
+++ contracts/defiedge/DefiEdgeVault.sol
@@ -0,0 +1,220 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity >=0.7.6;
+
+// import libraries
+import '@openzeppelin/contracts/math/SafeMath.sol';
+import '../libraries/DefiEdgeSharePriceProvider.sol';
+
+//  import interfaces
+import '../interfaces/IUnboundYieldWallet.sol';
+import '../interfaces/IUnboundYieldWalletFactory.sol';
+import '../interfaces/IDefiEdgeStrategy.sol';
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+
+// contracts
+import '../base/UnboundVaultBase.sol';
+
+import '../UnboundYieldWallet.sol';
+
+contract DefiEdgeVault is UnboundVaultBase {
+    using SafeMath for uint256;
+
+    bool[] public isBase;
+    uint256[] public decimals;
+    uint256 public maxPercentDiff;
+    uint256 public allowedDelay;
+
+    IDefiEdgeStrategy public strategy;
+
+    event Lock(address _user, uint256 _collateral, uint256 _uTokenAmount);
+    event Unlock(address _user, uint256 _collateral, uint256 _uTokenAmount);
+
+    /**
+     * @notice Creates new vault
+     * @param _uToken Address of the Unbound Token
+     * @param _governance Address of the governance
+     * @param _strategy Address of the defiedge strategy
+     * @param _stablecoin Address of the stablecoin
+     * @param _maxPercentDiff Percent deviation for oracle price. 1e8 is 100%
+     * @param _allowedDelay Allowed delay for Chainlink price update, in Epoch secondss
+     * @param _staking Address where the stake fees should be donated
+     */
+    constructor(
+        address _uToken,
+        address _governance,
+        address _strategy,
+        address _stablecoin,
+        uint256 _maxPercentDiff,
+        uint256 _allowedDelay,
+        address _staking
+    ) {
+        require(
+            _uToken != address(0) &&
+                _strategy != address(0) &&
+                _stablecoin != address(0),
+            'I'
+        );
+
+
+        uToken = IUnboundToken(_uToken);
+        governance = _governance;
+        strategy = IDefiEdgeStrategy(_strategy);
+        pair = IUniswapV2Pair(strategy.pool());
+
+        // decimals array
+        decimals.push(uint256(IERC20(pair.token0()).decimals()));
+        decimals.push(uint256(IERC20(pair.token1()).decimals()));
+
+        bool isBase0;
+        bool isBase1;
+
+        pair.token0() == _stablecoin ? isBase0 = true : isBase1 = true;
+
+        // push to isBase
+        isBase.push(isBase0);
+        isBase.push(isBase1);
+
+        maxPercentDiff = _maxPercentDiff;
+        allowedDelay = _allowedDelay;
+
+        staking = _staking;
+
+        factory = msg.sender;
+    }
+
+    /**
+     * @notice Lock pool tokens without permit
+     * @param _amount Amount of pool tokens to lock
+     * @param _mintTo Address to which the UND should be minted
+     * @param _minUTokenAmount Minimum uTokens to receive
+     */
+    function lock(
+        uint256 _amount,
+        address _mintTo,
+        uint256 _minUTokenAmount
+    ) external returns (uint256 amount) {
+        // lock pool tokens and mint amount
+        (amount) = _lock(_amount, _mintTo, _minUTokenAmount);
+
+        emit Lock(msg.sender, _amount, amount);
+    }
+
+    /**
+     * @notice Internal lock function to lock LPT
+     * @param _amount Amount to lock
+     * @param _mintTo Address to which the uTokens should be minted
+     * @param _minUTokenAmount Minimum number of uTokens to mint
+     */
+    function _lock(
+        uint256 _amount,
+        address _mintTo,
+        uint256 _minUTokenAmount
+    ) internal returns (uint256 amount) {
+        require(LTV != 0, 'NI');
+        // check if user has sufficient balance
+        require(strategy.balanceOf(msg.sender) >= _amount, 'BAL');
+
+        // transfer tokens to the vault contract
+        require(strategy.transferFrom(msg.sender, address(this), _amount), 'TF');
+
+        // get price of pool token from oracle
+        uint256 price = DefiEdgeSharePriceProvider.getSharePrice(
+            address(strategy),
+            decimals
+        );
+
+        // get total value in base asset
+        uint256 value = _amount.mul(uint256(price)).div(BASE);
+
+        amount = value.mul(LTV).div(SECOND_BASE);
+
+        collateral[msg.sender] = collateral[msg.sender].add(_amount);
+
+        (amount) = mint(msg.sender, amount, _mintTo);
+
+        require(_minUTokenAmount <= amount, 'MIN');
+
+    }
+
+    /**
+     * @notice Unlocks the pool tokens
+     *   To get 100% of the pool tokens back, user has burn 100% of uTokens
+     * @param _uTokenAmount Number of uTokens to burn
+     * @param _minCollateral Minimum collateral to give
+     */
+    function unlock(uint256 _uTokenAmount, uint256 _minCollateral)
+        external
+        returns (uint256 amount)
+    {
+        require(debt[msg.sender] >= _uTokenAmount, 'BAL');
+
+        // if user is returning 100% loan, give 100% of collateral
+        if (debt[msg.sender] == _uTokenAmount) {
+            amount = collateral[msg.sender];
+        } else {
+            amount = getTokensToReturn(_uTokenAmount);
+        }
+
+        collateral[msg.sender] = collateral[msg.sender].sub(amount);
+
+        burn(msg.sender, _uTokenAmount);
+
+        // give the pool tokens back
+        strategy.transfer(msg.sender, amount);
+
+        require(_minCollateral <= amount, 'MIN');
+
+        emit Unlock(msg.sender, amount, _uTokenAmount);
+    }
+
+    /**
+     * @notice Calculate return amount of collateral based on CR
+     * @param _uTokenAmount Amount of uToken to consider to return the collateral
+     */
+    function getTokensToReturn(uint256 _uTokenAmount)
+        internal
+        returns (uint256 amount)
+    {
+        require(CR != 0, 'NI');
+
+        // get price of pool token from oracle
+        uint256 price = DefiEdgeSharePriceProvider.getSharePrice(
+            address(strategy),
+            decimals
+        );
+        uint256 currentCR = uint256(price)
+            .mul(collateral[msg.sender])
+            .mul(SECOND_BASE)
+            .div(debt[msg.sender]);
+
+        // multiply by 1e26 to normalize it current CR (base for nomalization + 1e8 added in above step)
+        if (CR.mul(BASE) <= currentCR) {
+            // enough collateral
+            uint256 valueStart = uint256(price).mul(collateral[msg.sender]);
+            uint256 loanAfter = debt[msg.sender].sub(_uTokenAmount);
+            uint256 valueAfter = (CR.mul(loanAfter).mul(BASE)).div(SECOND_BASE);
+            amount = valueStart.sub(valueAfter).div(uint256(price));
+        } else {
+            // insufficient collateral
+            amount = (collateral[msg.sender].mul(_uTokenAmount)).div(
+                debt[msg.sender]
+            );
+        }
+    }
+
+    /**
+     * @notice Emergency unlock the tokens, burns the uTokens and unlocks LP Tokens
+     */
+    function emergencyUnlock() external {
+        require(
+            IERC20(address(uToken)).balanceOf(msg.sender) >= debt[msg.sender],
+            'BAL'
+        );
+        uint256 userDebt = debt[msg.sender];
+        uint256 userCollateral = collateral[msg.sender];
+        collateral[msg.sender] = 0; // prevent getting the same amount again
+        burn(msg.sender, userDebt);
+        strategy.transfer(msg.sender, userCollateral);
+        emit Unlock(msg.sender, userCollateral, userDebt);
+    }
+}
---
+++ contracts/defiedge/DefiEdgeVaultFactory.sol
@@ -0,0 +1,127 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity >=0.7.6;
+
+import '@openzeppelin/contracts/utils/Pausable.sol';
+
+import './DefiEdgeVault.sol';
+// libraries
+import '@openzeppelin/contracts/math/SafeMath.sol';
+
+// interfaces
+import '../interfaces/IERC20.sol';
+
+contract DefiEdgeVaultFactory is Pausable {
+    mapping(address => bool) public allowed;
+    mapping(address => bool) public vaults;
+
+    address public governance;
+    address public pendingGovernance;
+
+    uint256 public index;
+
+    mapping(uint256 => address) public vaultByIndex;
+
+    event NewVault(address _vault, uint256 _index);
+    event ChangeGovernance(address _governance);
+
+    event EnableVault(address _vault);
+    event DisableVault(address _vault);
+
+    modifier onlyGovernance() {
+        require(msg.sender == governance, 'NA');
+        _;
+    }
+
+    /**
+     * @param _governance Address of the governance
+     */
+    constructor(address _governance) {
+        governance = _governance;
+    }
+
+    /**
+     * @notice Creates new vault
+     * @param _uToken Address of the Unbound Token
+     * @param _governance Address of the governance
+     * @param _strategy Address of the defiedge strategy
+     * @param _stablecoin Address of the stablecoin
+     * @param _maxPercentDiff Percent deviation for oracle price. 1e8 is 100%
+     * @param _allowedDelay Allowed delay for Chainlink price update
+     * @param _staking Address where the stake fees should be donated
+     */
+    function createVault(
+        address _uToken,
+        address _governance,
+        address _strategy,
+        address _stablecoin,
+        uint256 _maxPercentDiff,
+        uint256 _allowedDelay,
+        address _staking
+    ) external whenNotPaused returns (address vault) {
+        vault = address(
+            new DefiEdgeVault(
+                _uToken,
+                _governance,
+                _strategy,
+                _stablecoin,
+                _maxPercentDiff,
+                _allowedDelay,
+                _staking
+            )
+        );
+        index = index + 1;
+        vaultByIndex[index] = vault;
+        vaults[vault] = true;
+        emit NewVault(vault, index);
+    }
+
+    /**
+     * @notice Enable vault so the vault can start minting uTokens
+     * @param _vault Address of the vault to enable
+     */
+    function enableVault(address _vault) external onlyGovernance {
+        require(vaults[_vault]);
+        allowed[_vault] = true;
+        emit EnableVault(_vault);
+    }
+
+    /**
+     * @notice Disables the vault to stop minting of uTokens
+     * @param _vault Address of the vault
+     */
+    function disableVault(address _vault) external onlyGovernance {
+        allowed[_vault] = false;
+        emit DisableVault(_vault);
+    }
+
+    /**
+     * @notice Changes governnance via two step process
+     * @param _governance Address of the new governance
+     */
+    function changeGovernance(address _governance) external onlyGovernance {
+        pendingGovernance = _governance;
+        emit ChangeGovernance(_governance);
+    }
+
+    /**
+     * @notice Accept governance role, should be called from pending governance
+     */
+    function acceptGovernance() external {
+        require(msg.sender == pendingGovernance, 'NA');
+        governance = pendingGovernance;
+    }
+
+    /**
+     * @notice Pause the mint and burn functionality
+     */
+    function setPause() external onlyGovernance {
+        _pause();
+    }
+
+    /**
+     * @notice Unpause the mint and burn functionality
+     */
+    function setUnpause() external onlyGovernance {
+        _unpause();
+    }
+}
