183143a1f2bb9cf6c3301f8a9cce6b8bbfac7f01
meetsiraja <meetsiraja@gmail.com>
2021-11-18 17:50:56 +0530

    ft. ape-swap farm yield wallet

---
+++ contracts/farming/apeswap/interfaces/IMasterChefApe.sol
@@ -0,0 +1,52 @@
+pragma solidity 0.7.6;
+pragma abicoder v2;
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+interface IMasterChefApe {
+
+    // Info of each pool.
+    struct PoolInfo {
+        IERC20 lpToken;           // Address of LP token contract.
+        uint256 allocPoint;       // How many allocation points assigned to this pool. SUSHIs to distribute per block.
+        uint256 lastRewardBlock;  // Last block number that SUSHIs distribution occurs.
+        uint256 accSushiPerShare; // Accumulated SUSHIs per share, times 1e12. See below.
+    }
+
+     // Info of each user.
+    struct UserInfo {
+        uint256 amount;     // How many LP tokens the user has provided.
+        uint256 rewardDebt; // Reward debt. See explanation below.
+        //
+        // We do some fancy math here. Basically, any point in time, the amount of SUSHIs
+        // entitled to a user but is pending to be distributed is:
+        //
+        //   pending reward = (user.amount * pool.accSushiPerShare) - user.rewardDebt
+        //
+        // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
+        //   1. The pool's `accSushiPerShare` (and `lastRewardBlock`) gets updated.
+        //   2. User receives the pending reward sent to his/her address.
+        //   3. User's `amount` gets updated.
+        //   4. User's `rewardDebt` gets updated.
+    }
+
+    function poolInfo(uint256 _pid) external view returns(PoolInfo memory);
+
+    // function getPoolInfo(uint256 _pid) external view returns(PoolInfo memory);
+
+    function userInfo(uint256 _pis, address _user) external view returns(UserInfo memory);
+
+    function pendingCake(uint256 _pid, address _user) external view returns (uint256);
+
+    function cake() external view returns (address);
+
+    function deposit(uint256 _pid, uint256 _amount) external;
+
+    function withdraw(uint256 _pid, uint256 _amount) external;
+
+    function enterStaking(uint256 _amount) external;
+
+    function leaveStaking(uint256 _amount) external;
+
+    
+}
